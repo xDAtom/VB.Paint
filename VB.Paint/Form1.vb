2f3f8dee3b536a244046d293d3bbea22f8e0dd02
xDAtom <os.kk.7b@gmail.com>
2014-04-19 18:38:24 +0200

    VB.Paint v1.0.2 (r0005)
    Fixes Issue 2. Edited About Description. Fixed Status Bar always showing
    "Ready". Open and Save functions have been mastered.

--- VB.Paint/Form1.vb
+++ VB.Paint/Form1.vb
@@ -7,10 +7,13 @@
     Dim bgbrush = Brushes.CornflowerBlue
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        StatusLabel.Text = "Initializing..."
         Me.Image.Image = New Bitmap(Me.Image.Width, Me.Image.Height)
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub NewShortcut_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NewShortcut.Click
+        StatusLabel.Text = "Creating New Drawing..."
         Image.CreateGraphics.FillRectangle(whitebrush, 0, 0, 673, 473)
         Image.Enabled = True
         Image.BackColor = Color.White
@@ -18,9 +21,11 @@
         OpenShortcut.Enabled = True
         SaveButton.Enabled = True
         SaveShortcut.Enabled = True
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub NewButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NewButton.Click
+        StatusLabel.Text = "Creating New Drawing..."
         Image.CreateGraphics.FillRectangle(whitebrush, 0, 0, 673, 473)
         Image.Enabled = True
         Image.BackColor = Color.White
@@ -28,9 +33,11 @@
         OpenShortcut.Enabled = True
         SaveButton.Enabled = True
         SaveShortcut.Enabled = True
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub BrushSize_SelectedItemChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BrushSize.SelectedItemChanged
+        StatusLabel.Text = "Changing Brush Size..."
         On Error GoTo brushsizehandler
         Dim pensize, coord As Single
         If BrushSize.Text = "" Then
@@ -40,6 +47,7 @@
         BrushPreview.CreateGraphics.FillRectangle(bgbrush, 0, 0, 222, 222)
         coord = 111 - (pensize / 2)
         BrushPreview.CreateGraphics.FillEllipse(brush, coord, coord, pensize, pensize)
+        StatusLabel.Text = "Ready"
         Exit Sub
 brushsizehandler:
         MsgBox("Crashed - String not accepted in Brush Size TextBox!", MsgBoxStyle.Critical, "Error")
@@ -52,11 +60,13 @@ brushsizehandler:
 
     Private Sub Image_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Image.MouseMove
         On Error GoTo brushdrawhandler
+        StatusLabel.Text = "Drawing..."
         Dim pensize As Single
         pensize = BrushSize.Text
         If draw = True Then
             Image.CreateGraphics.FillEllipse(brush, e.X - (pensize / 2), e.Y - (pensize / 2), pensize, pensize)
         End If
+        StatusLabel.Text = "Ready"
         Exit Sub
 brushdrawhandler:
         MsgBox("Crashed - String not accepted in Brush Size TextBox!", MsgBoxStyle.Critical, "Error")
@@ -68,92 +78,113 @@ brushdrawhandler:
     End Sub
 
     Private Sub BrushColorRed_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BrushColorRed.Click
+        StatusLabel.Text = "Changing Color..."
         brush = Brushes.Red
         Dim pensize, coord As Single
         pensize = BrushSize.Text
         BrushPreview.CreateGraphics.FillRectangle(bgbrush, 0, 0, 222, 222)
         coord = 111 - (pensize / 2)
         BrushPreview.CreateGraphics.FillEllipse(brush, coord, coord, pensize, pensize)
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub BrushColorGreen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BrushColorGreen.Click
+        StatusLabel.Text = "Changing Color..."
         brush = Brushes.Lime
         Dim pensize, coord As Single
         pensize = BrushSize.Text
         BrushPreview.CreateGraphics.FillRectangle(bgbrush, 0, 0, 222, 222)
         coord = 111 - (pensize / 2)
         BrushPreview.CreateGraphics.FillEllipse(brush, coord, coord, pensize, pensize)
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub BrushColorBlue_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BrushColorBlue.Click
+        StatusLabel.Text = "Changing Color..."
         brush = Brushes.Blue
         Dim pensize, coord As Single
         pensize = BrushSize.Text
         BrushPreview.CreateGraphics.FillRectangle(bgbrush, 0, 0, 222, 222)
         coord = 111 - (pensize / 2)
         BrushPreview.CreateGraphics.FillEllipse(brush, coord, coord, pensize, pensize)
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub BrushColorYellow_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BrushColorYellow.Click
+        StatusLabel.Text = "Changing Color..."
         brush = Brushes.Yellow
         Dim pensize, coord As Single
         pensize = BrushSize.Text
         BrushPreview.CreateGraphics.FillRectangle(bgbrush, 0, 0, 222, 222)
         coord = 111 - (pensize / 2)
         BrushPreview.CreateGraphics.FillEllipse(brush, coord, coord, pensize, pensize)
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub BrushColorMagenta_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BrushColorMagenta.Click
+        StatusLabel.Text = "Changing Color..."
         brush = Brushes.Magenta
         Dim pensize, coord As Single
         pensize = BrushSize.Text
         BrushPreview.CreateGraphics.FillRectangle(bgbrush, 0, 0, 222, 222)
         coord = 111 - (pensize / 2)
         BrushPreview.CreateGraphics.FillEllipse(brush, coord, coord, pensize, pensize)
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub BrushColorCyan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BrushColorCyan.Click
+        StatusLabel.Text = "Changing Color..."
         brush = Brushes.Cyan
         Dim pensize, coord As Single
         pensize = BrushSize.Text
         BrushPreview.CreateGraphics.FillRectangle(bgbrush, 0, 0, 222, 222)
         coord = 111 - (pensize / 2)
         BrushPreview.CreateGraphics.FillEllipse(brush, coord, coord, pensize, pensize)
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub BrushColorBlack_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BrushColorBlack.Click
+        StatusLabel.Text = "Changing Color..."
         brush = Brushes.Black
         Dim pensize, coord As Single
         pensize = BrushSize.Text
         BrushPreview.CreateGraphics.FillRectangle(bgbrush, 0, 0, 222, 222)
         coord = 111 - (pensize / 2)
         BrushPreview.CreateGraphics.FillEllipse(brush, coord, coord, pensize, pensize)
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub BrushColorWhite_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BrushColorWhite.Click
+        StatusLabel.Text = "Changing Color..."
         brush = Brushes.White
         Dim pensize, coord As Single
         pensize = BrushSize.Text
         BrushPreview.CreateGraphics.FillRectangle(bgbrush, 0, 0, 222, 222)
         coord = 111 - (pensize / 2)
         BrushPreview.CreateGraphics.FillEllipse(brush, coord, coord, pensize, pensize)
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub BrushColorOrange_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BrushColorOrange.Click
+        StatusLabel.Text = "Changing Color..."
         brush = Brushes.Orange
         Dim pensize, coord As Single
         pensize = BrushSize.Text
         BrushPreview.CreateGraphics.FillRectangle(bgbrush, 0, 0, 222, 222)
         coord = 111 - (pensize / 2)
         BrushPreview.CreateGraphics.FillEllipse(brush, coord, coord, pensize, pensize)
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub BrushPreview_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BrushPreview.LoadCompleted
+        StatusLabel.Text = "Initializing Brush..."
         brush = Brushes.Black
         BrushPreview.CreateGraphics.FillEllipse(brush, 110, 110, 1, 1)
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub CloseShortcut_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CloseShortcut.Click
+        StatusLabel.Text = "Closing Drawing..."
         Image.CreateGraphics.FillRectangle(controlbrush, 0, 0, 673, 473)
         Image.BackgroundImage = Nothing
         Image.Image = Nothing
@@ -163,9 +194,11 @@ brushdrawhandler:
         OpenShortcut.Enabled = False
         SaveButton.Enabled = False
         SaveShortcut.Enabled = False
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub CloseButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CloseButton.Click
+        StatusLabel.Text = "Closing Drawing..."
         Image.CreateGraphics.FillRectangle(controlbrush, 0, 0, 673, 473)
         Image.BackgroundImage = Nothing
         Image.Image = Nothing
@@ -175,31 +208,39 @@ brushdrawhandler:
         OpenShortcut.Enabled = False
         SaveButton.Enabled = False
         SaveShortcut.Enabled = False
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub ExitButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitButton.Click
+        StatusLabel.Text = "Exiting..."
         End
     End Sub
 
     Private Sub HelpButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HelpBtn.Click
         On Error GoTo 404
+        StatusLabel.Text = "Starting readme..."
         System.Diagnostics.Process.Start("readme.txt")
+        StatusLabel.Text = "Ready"
         Exit Sub
 404:
         MsgBox("Cannot find readme.txt", MsgBoxStyle.SystemModal, "Error")
     End Sub
 
     Private Sub AboutButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutButton.Click
+        StatusLabel.Text = "Showing About Window..."
         Dim SecondForm As AboutBox1 = AboutBox1
         SecondForm.Show()
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub OpenButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OpenButton.Click
+        On Error GoTo nonimage1
         If Image.Enabled = False Then
             OpenShortcut.Enabled = False
             OpenButton.Enabled = False
             Exit Sub
         End If
+        StatusLabel.Text = "Opening Image..."
         OpenFileDialog.Filter = "All Files|*.*"
         OpenFileDialog.FileName = ""
         If OpenFileDialog.ShowDialog(Me) = DialogResult.OK Then
@@ -207,14 +248,20 @@ brushdrawhandler:
             Image.BackgroundImage = System.Drawing.Bitmap.FromFile(img)
             Image.BackgroundImageLayout = ImageLayout.Stretch
         End If
+        StatusLabel.Text = "Ready"
+        Exit Sub
+nonimage1:
+        MsgBox("Please open an Image File.", MsgBoxStyle.Critical, "Error")
     End Sub
 
     Private Sub OpenShortcut_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OpenShortcut.Click
+        On Error GoTo nonimage2
         If Image.Enabled = False Then
             OpenButton.Enabled = False
             OpenShortcut.Enabled = False
             Exit Sub
         End If
+        StatusLabel.Text = "Opening Image..."
         OpenFileDialog.Filter = "All Files|*.*"
         OpenFileDialog.FileName = ""
         If OpenFileDialog.ShowDialog(Me) = DialogResult.OK Then
@@ -222,9 +269,14 @@ brushdrawhandler:
             Image.BackgroundImage = System.Drawing.Bitmap.FromFile(img)
             Image.BackgroundImageLayout = ImageLayout.Stretch
         End If
+        StatusLabel.Text = "Ready"
+        Exit Sub
+nonimage2:
+        MsgBox("Please open an Image File.", MsgBoxStyle.Critical, "Error")
     End Sub
 
     Private Sub SaveShortcut_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SaveShortcut.Click
+        StatusLabel.Text = "Saving Drawing..."
         Dim bounds As Rectangle
         Dim screenshot As System.Drawing.Bitmap
         Dim graph As Graphics
@@ -239,9 +291,11 @@ brushdrawhandler:
         If SaveFileDialog.ShowDialog() = DialogResult.OK Then
             Image.Image.Save(SaveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Bmp)
         End If
+        StatusLabel.Text = "Ready"
     End Sub
 
     Private Sub SaveButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SaveButton.Click
+        StatusLabel.Text = "Saving Drawing..."
         Dim bounds As Rectangle
         Dim screenshot As System.Drawing.Bitmap
         Dim graph As Graphics
@@ -256,5 +310,6 @@ brushdrawhandler:
         If SaveFileDialog.ShowDialog() = DialogResult.OK Then
             Image.Image.Save(SaveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Bmp)
         End If
+        StatusLabel.Text = "Ready"
     End Sub
 End Class
--- VB.Paint/My Project/AssemblyInfo.vb
+++ VB.Paint/My Project/AssemblyInfo.vb
@@ -9,7 +9,7 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("VB.Paint")> 
-<Assembly: AssemblyDescription("VB.Paint is a lightweight image editing program for fun! And yes I know it's crap. Coded in VB2008, it can only draw an 673x473 image with a brush that is customizable in size and 9 colors. Due to the limitations of VB2008, it CANNOT open nor save image files created in VB.Paint. Also it can only show 9 colors: Red, Green, Blue, Magenta, Yellow, Cyan, Black, White, and Orange because stupid VB2008 cannot edit the brush in the RGB way. And hard-coding all of the 16.5M colors would make this <500kB project into a >50MB one. So enjoy this as it is my first program. ;)")> 
+<Assembly: AssemblyDescription("VB.Paint is a lightweight image editing program for fun! And yes I know it's crap. Coded in VB2008, it can only draw an 673x473 image with a brush that is customizable in size and 9 colors. Due to the limitations of VB2008, it can only draw 9 colors: Red, Green, Blue, Magenta, Yellow, Cyan, Black, White, and Orange instead of RGB colors. Hard-coding all of the 16.5M colors would make this <500kB project into a >50MB one, so I'm not planning on fixing that YET. It can display all RGB colors if they are IMPORTED FROM AN EXTERNAL IMAGE from the Open Function. Anyways, enjoy this as it is my first program. ;)")> 
 <Assembly: AssemblyCompany("xDAtom Productions")> 
 <Assembly: AssemblyProduct("VB.Paint")> 
 <Assembly: AssemblyCopyright("Copyright ©  2014")> 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.1.1")> 
-<Assembly: AssemblyFileVersion("1.0.1.1")> 
+<Assembly: AssemblyVersion("1.0.2.0")> 
+<Assembly: AssemblyFileVersion("1.0.2.0")> 
